095d769eb6eb46766964e945bc27b7a74d220cda
Ale Delgado Martel <alejandrodelgadomartel@gmail.com>
2017-03-20 00:20:01 +0000

    captura de imagenes cada X segundos

--- tests/captura_imagenes.cpp
+++ tests/captura_imagenes.cpp
@@ -6,6 +6,8 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <thread>
+
 #include <unistd.h>
 
 #include <cstring>
@@ -20,70 +22,94 @@ using namespace std;
 cv::VideoCapture video;
 cv::Mat bgrMap;
 
-int main(int argc, char* argv[]){
-  char key = 0;
-
-  
-  int segundos = atoi(argv[1]);
- 
-  
-
-  int numSnapshot = 0;
+int numSnapshot = 0;
   std::string id = "0";
-  std::cout << "Press 'Esc' to exit" << std::endl;
 
-  video.open(0);
+int segundos;
 
-  if (!video.isOpened()) {
-    std::cerr<<"Could not open video"<<std::endl;
-    return -1;
-  }
-  
-  std::cout << segundos << std::endl;
+time_t rawtime;
+struct tm * timeinfo;
 
-  while (key!=27 && video.grab()) {
-	
-	time_t rawtime;
-	struct tm * timeinfo;
-	
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-	
-	
-	
-	std::string hora = "";
-	  
-	  
-	  
-    video.retrieve(bgrMap);
+std::string hora;
+char key = 0;
 
-    cv::imshow("BGR image", bgrMap);
-    
-    
-    
 
-    //if (key == 27) { //CADA x (ARGV[0]) SECS SACAR FOTO
-		
-	  for(int i=11; i<19; i++){
-			  hora += asctime(timeinfo)[i];
-	  }
-	  
-	  std::cout << hora << std::endl;
-		
-      cv::imwrite(id + "-" + hora + ".png", bgrMap);
-      numSnapshot++;
-      id = static_cast<std::ostringstream*>(&(std::ostringstream() << numSnapshot))->str();
-      
-      
-		  
-	  
-	  
-     //}
-     
-     sleep(segundos);
 
-  key=cv::waitKey(20);
-  
-  
-  }
+
+void captura(){
+
+
+    while(key != 27){
+
+    std::cout << hora << std::endl;
+
+    cv::imwrite(id + "-" + hora + ".png", bgrMap);
+    numSnapshot++;
+    id = static_cast<std::ostringstream*>(&(std::ostringstream() << numSnapshot))->str();
+
+    sleep(segundos);
+
+    }
+
+}
+
+
+int graba(){
+    std::cout << "Press 'Esc' to exit" << std::endl;
+
+    video.open(0);
+
+    if (!video.isOpened()) {
+        std::cerr<<"Could not open video"<<std::endl;
+        return -1;
+    }
+
+
+    while (key!=27 && video.grab()) {
+
+
+
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+
+
+
+        hora = "";
+        for(int i=11; i<19; i++){
+          hora += asctime(timeinfo)[i];
+        }
+
+
+
+        video.retrieve(bgrMap);
+
+        cv::imshow("BGR image", bgrMap);
+
+
+        key=cv::waitKey(20);
+
+
+    }
+
+
+
+}
+
+
+
+int main(int argc, char* argv[]){
+
+
+
+    segundos = atoi(argv[1]);
+
+    std::thread foto(captura);
+    std::thread video(graba);
+
+
+
+    foto.join();
+    video.join();
+
+
 }
