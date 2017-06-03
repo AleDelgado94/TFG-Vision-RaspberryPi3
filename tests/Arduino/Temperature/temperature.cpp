d51603ddfb0c45e525897e578955c3d14184c2d8
Ale Delgado Martel <alejandrodelgadomartel@gmail.com>
2017-06-03 12:30:48 +0100

    arreglado consultas datos

--- tests/Arduino/Temperature/temperature.cpp
+++ tests/Arduino/Temperature/temperature.cpp
@@ -5,6 +5,7 @@
 #include <boost/program_options/option.hpp>
 #include <sqlite3.h>
 #include "../../Database/sqlite3/sqliteCallBack.hpp"
+#include <list>
 
 using namespace::boost::asio;
 using namespace std;
@@ -29,49 +30,110 @@ string read_serial(serial_port& port){
 }
 
 
-std::vector<result_query> consulta_datos(sqlite3* db, char* consulta){
+std::list<result_query>* consulta_datos(sqlite3* db, char* consulta){
   int handler;
   sqlite3_stmt* ppStmt;
-  std::vector<result_query> resultado;
-  result_query fila;
+  std::list<result_query>* resultado = new std::list<result_query>();
   handler = sqlite3_prepare_v2(db, consulta, -1, &ppStmt, NULL);
   if(handler != SQLITE_OK){
     cerr << "Error al ejecutar la consulta" << endl;
   }else{
-    while(SQLITE_ROW == sqlite3_step(ppStmt)){
-      fila.id_name = (sqlite3_column_text(ppStmt, 0));
-      fila.hora = (sqlite3_column_text(ppStmt, 1));
-      fila.fecha = (sqlite3_column_text(ppStmt, 2));
-      fila.temperatura = sqlite3_column_double(ppStmt, 3);
-      fila.humedad = sqlite3_column_double(ppStmt, 4);
-      fila.celula_fotoelectrica = sqlite3_column_double(ppStmt, 5);
-      fila.estado = (sqlite3_column_text(ppStmt, 6));
+    int i=0;
+    const unsigned char* vacio = reinterpret_cast<const unsigned char*>("");
 
-      resultado.push_back(fila);
+    while(SQLITE_ROW == sqlite3_step(ppStmt)){
+      result_query fila;
+
+      if(sqlite3_column_text(ppStmt, 0) == NULL){
+        fila.setId_name(vacio);
+      }else{
+        fila.setId_name(sqlite3_column_text(ppStmt, 0));
+      }
+
+
+      if(sqlite3_column_text(ppStmt, 1) == NULL){
+        fila.setHora(vacio);
+      }else{
+        fila.setHora(sqlite3_column_text(ppStmt, 1));
+      }
+
+
+      if(sqlite3_column_text(ppStmt, 2) == NULL){
+        fila.setFecha(vacio);
+      }else{
+        fila.setFecha(sqlite3_column_text(ppStmt, 2));
+      }
+
+
+      if(sqlite3_column_text(ppStmt, 6) == NULL){
+        fila.setEstado(vacio);
+      }else{
+        fila.setEstado(sqlite3_column_text(ppStmt, 6));
+      }
+
+
+      fila.setTemperatura(sqlite3_column_double(ppStmt, 3));
+      fila.setHumedad(sqlite3_column_double(ppStmt, 4));
+      fila.setCelula_fotoelectrica(sqlite3_column_double(ppStmt, 5));
+
+      resultado->push_back(fila);
+      i++;
+
     }
   }
   return resultado;
 }
 
-std::vector<result_query> consulta_datos(sqlite3* db, std::string consulta){
+
+std::list<result_query>* consulta_datos(sqlite3* db, std::string consulta){
   int handler;
   sqlite3_stmt* ppStmt;
-  std::vector<result_query> resultado;
-  result_query fila;
+  std::list<result_query>* resultado = new std::list<result_query>();
   handler = sqlite3_prepare_v2(db, consulta.c_str(), -1, &ppStmt, NULL);
   if(handler != SQLITE_OK){
     cerr << "Error al ejecutar la consulta" << endl;
   }else{
-    while(SQLITE_ROW == sqlite3_step(ppStmt)){
-      fila.id_name = (sqlite3_column_text(ppStmt, 0));
-      fila.hora = (sqlite3_column_text(ppStmt, 1));
-      fila.fecha = (sqlite3_column_text(ppStmt, 2));
-      fila.temperatura = sqlite3_column_double(ppStmt, 3);
-      fila.humedad = sqlite3_column_double(ppStmt, 4);
-      fila.celula_fotoelectrica = sqlite3_column_double(ppStmt, 5);
-      fila.estado = (sqlite3_column_text(ppStmt, 6));
+    int i=0;
+    const unsigned char* vacio = reinterpret_cast<const unsigned char*>("");
 
-      resultado.push_back(fila);
+    while(SQLITE_ROW == sqlite3_step(ppStmt)){
+      result_query fila;
+
+      if(sqlite3_column_text(ppStmt, 0) == NULL){
+        fila.setId_name(vacio);
+      }else{
+        fila.setId_name(sqlite3_column_text(ppStmt, 0));
+      }
+
+
+      if(sqlite3_column_text(ppStmt, 1) == NULL){
+        fila.setHora(vacio);
+      }else{
+        fila.setHora(sqlite3_column_text(ppStmt, 1));
+      }
+
+
+      if(sqlite3_column_text(ppStmt, 2) == NULL){
+        fila.setFecha(vacio);
+      }else{
+        fila.setFecha(sqlite3_column_text(ppStmt, 2));
+      }
+
+
+      if(sqlite3_column_text(ppStmt, 6) == NULL){
+        fila.setEstado(vacio);
+      }else{
+        fila.setEstado(sqlite3_column_text(ppStmt, 6));
+      }
+
+
+      fila.setTemperatura(sqlite3_column_double(ppStmt, 3));
+      fila.setHumedad(sqlite3_column_double(ppStmt, 4));
+      fila.setCelula_fotoelectrica(sqlite3_column_double(ppStmt, 5));
+
+      resultado->push_back(fila);
+      i++;
+
     }
   }
   return resultado;
@@ -111,39 +173,25 @@ void ask_data(serial_port& port, sqlite3* db){
   sql += ",";
   sql += to_string(cel);
   sql += ");";
-  cout << sql << endl;
-  sqlite3_exec(db, sql.c_str(), callback, 0, &sqlError);
+  int correct = sqlite3_exec(db, sql.c_str(), callback, 0, &sqlError);
+  if(correct != SQLITE_OK)
+    std::cerr << "Error: " << sqlError  << '\n';
 }
 
 
 int main(int argc, char const *argv[]){
   io_service io;
   string PORT;
+  std::string database;
   sqlite3* db;
   char* sqError = 0;
-
-  int db_handler = sqlite3_open("Database/database.db", &db);
-  if(db_handler){
-      std::cerr << "No se puede abrir la base de datos. Error: " << sqlite3_errmsg(db) <<'\n';
-      return(-1);
-  }else{
-    cout << "Se abrió la base de datos" << endl;
-  }
-
-  string crear_tabla_data = "CREATE TABLE IF NOT EXISTS DATOS ( ID_NOMBRE TEXT PRIMARY KEY, HORA TEXT, FECHA TEXT,  TEMPERATURA INTEGER, HUMEDAD INTEGER, SOLAR REAL, ESTADO TEXT);";
-  int exe = sqlite3_exec(db, crear_tabla_data.c_str(), callback, 0, &sqError);
-  if(exe != SQLITE_OK){
-    fprintf(stderr, "SQL error: %s\n", sqError);
-    sqlite3_free(sqError);
-  }else{
-    std::cout << "Tabla creada con exito" << '\n';
-  }
 
 
   opt::options_description desc("Options");
   desc.add_options()
     ("help,h", "./Temperature [[ --port | -p ] puerto]")
-    ("port,p", opt::value<std::vector<std::string>>(), "puerto");
+    ("port,p", opt::value<std::vector<std::string>>(), "puerto")
+    ("database,d", opt::value<std::vector<std::string>>(), "base de datos");
 
     opt::variables_map vm;
     opt::positional_options_description p;
@@ -164,6 +212,30 @@ int main(int argc, char const *argv[]){
       std::vector<std::string> v = vm["port"].as<std::vector<std::string>>();
       PORT = v[0];
     }
+    if(vm.count("database")){
+      std::vector<std::string> v = vm["database"].as<std::vector<std::string>>();
+      database = v[0];
+    }
+
+
+    int db_handler = sqlite3_open(database.c_str(), &db);
+    if(db_handler){
+        std::cerr << "No se puede abrir la base de datos. Error: " << sqlite3_errmsg(db) <<'\n';
+        return(-1);
+    }else{
+      cout << "Se abrió la base de datos" << endl;
+    }
+
+    string crear_tabla_data = "CREATE TABLE IF NOT EXISTS DATOS ( ID_NOMBRE TEXT PRIMARY KEY, HORA TEXT, FECHA TEXT,  TEMPERATURA INTEGER, HUMEDAD INTEGER, SOLAR REAL, ESTADO TEXT);";
+    int exe = sqlite3_exec(db, crear_tabla_data.c_str(), callback, 0, &sqError);
+    if(exe != SQLITE_OK){
+      fprintf(stderr, "SQL error: %s\n", sqError);
+      sqlite3_free(sqError);
+    }else{
+      std::cout << "Tabla creada con exito" << '\n';
+    }
+
+
 
     serial_port port( io, PORT.c_str() );
     port.set_option( BAUD );
@@ -173,31 +245,34 @@ int main(int argc, char const *argv[]){
 
     ask_data(port, db);
     string consult = "SELECT * FROM DATOS;";
-    std::vector<result_query> v = consulta_datos(db, consult);
+    std::list<result_query>* l_original = consulta_datos(db, consult);
+    std::list<result_query>* l = l_original;
 
-    for (size_t i = 0; i < v.size(); i++) {
-      cout << v[i].id_name << endl;
-      cout << v[i].hora << endl;
-      cout << v[i].fecha << endl;
-      cout << v[i].temperatura << endl;
-      cout << v[i].humedad << endl;
-      cout << v[i].celula_fotoelectrica << endl;
-      cout << v[i].estado << endl;
+    std::cout << "Size lista original: " << l_original->size() << '\n';
+    std::cout << "Size lista copia: " << l->size() << '\n';
+
+
+    while(!l->empty()){
+
+      result_query fila = l->front();
+
+      std::cout << "------------------------------" << '\n';
+      cout << fila.getId_name() << endl;
+      cout << fila.getHora() << endl;
+      cout << fila.getFecha() << endl;
+      cout << fila.getTemperatura() << endl;
+      cout << fila.getHumedad() << endl;
+      cout << fila.getCelula_fotoelectrica() << endl;
+      cout << fila.getEstado() << endl;
+      std::cout << "------------------------------" << '\n';
+
+      l->pop_front();
+
     }
 
-    /*for (std::vector<result_query>::iterator it = v.begin(); it != v.end(); ++it) {
-      cout << it.id_name << endl;
-      cout << it.hora << endl;
-      cout << it.fecha << endl;
-      cout << it.temperatura << endl;
-      cout << it.humedad << endl;
-      cout << it.celula_fotoelectrica << endl;
-      cout << it.estado << endl;
-
-    }*/
   unsigned char input[1];
-  /*
-  do{
+
+/*  do{
       cin >> input[0];
 
 
--- tests/Database/sqlite3/sqliteCallBack.hpp
+++ tests/Database/sqlite3/sqliteCallBack.hpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-
+/*
 struct result_query{
   const unsigned char* id_name;
   const unsigned char* hora;
@@ -13,10 +13,41 @@ struct result_query{
   float humedad;
   float celula_fotoelectrica;
   const unsigned char* estado;
+};*/
+
+
+class result_query {
+private:
+  const unsigned char* id_name;
+  const unsigned char* hora;
+  const unsigned char* fecha;
+  float temperatura;
+  float humedad;
+  float celula_fotoelectrica;
+  const unsigned char* estado;
+
+public:
+  result_query (){};
+  virtual ~result_query (){};
+
+  const unsigned char *getId_name() const;
+  void setId_name(const unsigned char *value);
+  const unsigned char *getHora() const;
+  void setHora(const unsigned char *value);
+  const unsigned char *getFecha() const;
+  void setFecha(const unsigned char *value);
+  float getTemperatura() const;
+  void setTemperatura(float value);
+  float getHumedad() const;
+  void setHumedad(float value);
+  float getCelula_fotoelectrica() const;
+  void setCelula_fotoelectrica(float value);
+  const unsigned char *getEstado() const;
+  void setEstado(const unsigned char *value);
 };
 
 static int callback(void *data, int argc, char **argv, char **azColName){
-   int i;
+    int i;
    fprintf(stderr, "%s: ", (const char*)data);
    for(i=0; i<argc; i++){
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
@@ -46,3 +77,73 @@ static int callback_one_data(void *data, int argc, char **argv, char **azColName
    std::vector<result_query> &data = (std::vector<result_query>)query;
    return 0;
 }*/
+
+const unsigned char *result_query::getHora() const
+{
+return hora;
+}
+
+void result_query::setHora(const unsigned char *value)
+{
+hora = value;
+}
+
+const unsigned char *result_query::getFecha() const
+{
+return fecha;
+}
+
+void result_query::setFecha(const unsigned char *value)
+{
+fecha = value;
+}
+
+float result_query::getTemperatura() const
+{
+return temperatura;
+}
+
+void result_query::setTemperatura(float value)
+{
+temperatura = value;
+}
+
+float result_query::getHumedad() const
+{
+return humedad;
+}
+
+void result_query::setHumedad(float value)
+{
+humedad = value;
+}
+
+float result_query::getCelula_fotoelectrica() const
+{
+return celula_fotoelectrica;
+}
+
+void result_query::setCelula_fotoelectrica(float value)
+{
+celula_fotoelectrica = value;
+}
+
+const unsigned char *result_query::getEstado() const
+{
+return estado;
+}
+
+void result_query::setEstado(const unsigned char *value)
+{
+estado = value;
+}
+
+const unsigned char *result_query::getId_name() const
+{
+return id_name;
+}
+
+void result_query::setId_name(const unsigned char *value)
+{
+id_name = value;
+}
